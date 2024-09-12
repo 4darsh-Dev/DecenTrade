538a2d13d27927033656ca1172d813a1204fa23e
Adarsh <adarshmaurya9415@gmail.com>
2024-09-12 12:59:21 +0530

    compiled issue fixed

--- smart-contracts/contracts/Marketplace.sol
+++ smart-contracts/contracts/Marketplace.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "./Counters.sol";
+// import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
