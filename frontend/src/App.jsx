6da936d8b24b8e975aa3494a9c038397eb99325a
Aman Verma <amanver125@gmail.com>
2024-10-22 16:58:14 +0530

    Update App.jsx
    #119

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,33 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { useEffect, useState } from 'react'
-import HomePage from './pages/HomePage'
-import NFTMarketplace from './components/NFTMarketplace'
-import ExplorePage from './pages/ExplorePage'
-import AboutPage from './pages/AboutPage'
-import CreatorsPage from './pages/CreatorsPage'
-import FAQPage from './pages/FAQPage'
-import Navbar from './components/Navbar'
-import CreateNFT from './pages/CreateNFTPage'
-import NotFoundPage from './components/NotFoundPage'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'; 
+import { useEffect, useState } from 'react';
+import HomePage from './pages/HomePage';
+import NFTMarketplace from './components/NFTMarketplace';
+import ExplorePage from './pages/ExplorePage';
+import AboutPage from './pages/AboutPage';
+import CreatorsPage from './pages/CreatorsPage';
+import FAQPage from './pages/FAQPage';
+import Navbar from './components/Navbar';
+import CreateNFT from './pages/CreateNFTPage';
+import NotFoundPage from './components/NotFoundPage';
+import Footer from './components/Footer'; // Import Footer component
+import MyChatbot from './Chatbot'; // Import Chatbot component
+
 function App() {
-    const [wallet, setWallet] = useState(null)
+    const [wallet, setWallet] = useState(null);
+
     useEffect(() => {
-        console.log(wallet)
-    }, [wallet])
+        console.log(wallet);
+    }, [wallet]);
+
     return (
         <BrowserRouter>
-            <div className="min-h-screen">
-                <div className="">
-                    <Navbar wallet={wallet} setWallet={setWallet} />
+            <div className="min-h-screen flex flex-col">
+                <Navbar wallet={wallet} setWallet={setWallet} />
+                <main className="flex-grow">
                     <Routes>
                         <Route
                             path="/"
-                            element={
-                                <HomePage
-                                    wallet={wallet}
-                                    setWallet={setWallet}
-                                />
-                            }
+                            element={<HomePage wallet={wallet} setWallet={setWallet} />}
                         />
                         <Route
                             path="/marketplace"
@@ -41,13 +41,18 @@ function App() {
                         <Route path="/about" element={<AboutPage />} />
                         <Route path="/creators" element={<CreatorsPage />} />
                         <Route path="/faqs" element={<FAQPage />} />
-                        <Route path="/create" element={<CreateNFT />} />
                         <Route path="*" element={<NotFoundPage />} />
                     </Routes>
-                </div>
+                </main>
+
+                {/* Add Chatbot component at the bottom of the page */}
+                <MyChatbot />
+
+                {/* Add Footer component */}
+                <Footer />
             </div>
         </BrowserRouter>
-    )
+    );
 }
 
-export default App
+export default App;
