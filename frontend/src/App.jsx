43009a9bf439fbe78547270e4c8c8e3ebdbd5a70
sezallagwal <sezal1470@gmail.com>
2024-10-25 15:55:11 +0530

    added multilanguage support

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,7 @@ import CreateNFT from './pages/CreateNFTPage'
 import NotFoundPage from './components/NotFoundPage'
 import Footer from './components/Footer'
 import CustomCursor from './components/CustomCursor'
+import GTranslateLoader from './components/GTranslateLoader'
 
 function App() {
     const [wallet, setWallet] = useState(null)
@@ -51,6 +52,7 @@ function App() {
                     <Footer />
                 </div>
             </div>
+            <GTranslateLoader />
         </BrowserRouter>
     )
 }
